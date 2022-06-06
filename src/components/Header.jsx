5e583d76ca3db621b0dea416f5ddc77239598e15
Archoza <geofiziks@mail.ru>
2022-06-06 15:53:28 +0300

    start project

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324"
+                alt=""/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className={'content'}>Main Content
+            <div>
+                <img
+                    src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
+                    alt=""/>
+            </div>
+            <div>ava + desc</div>
+
+            <div>My posts
+                <div>New post </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                    <div>Post 3</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
