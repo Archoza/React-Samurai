68631c524abf8835a8916bb257b00d5ae2798e11
Archoza <geofiziks@mail.ru>
2022-06-06 15:58:28 +0300

    change Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <header className={'header'}>
+            <h1>Hello samurai</h1>
             <img
                 src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324"
                 alt=""/>
