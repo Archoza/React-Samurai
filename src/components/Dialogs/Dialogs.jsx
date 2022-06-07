55e3006fa94113ec32c422490ee29f266572ac42
Archoza <geofiziks@mail.ru>
2022-06-07 21:48:48 +0300

    21 layout Dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,23 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>Dialogs</div>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>Vlad</div>
+                <div className={classes.dialog}>Alina</div>
+                <div className={classes.dialog}>Roma</div>
+                <div className={classes.dialog}>Igor</div>
+                <div className={classes.dialog}>Viktor</div>
+                <div className={classes.dialog}>Andrey</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>hi</div>
+                <div className={classes.message}>how is your it?</div>
+                <div className={classes.message}>Yo!</div>
+
+            </div>
+        </div>
+
 
     )
 }
