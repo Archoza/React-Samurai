fb32c0f288444b1e51ba0d761b96ea219276d6fb
Archoza <geofiziks@mail.ru>
2022-06-20 22:05:17 +0300

    52 clear function

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,11 +4,14 @@ import axios from "axios";
 
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items)
-            })
+    let getUsers = () =>
+    {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
 
 
@@ -40,6 +43,7 @@ const Users = (props) => {
     ))
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             <div>{userData}</div>
         </div>
     )
