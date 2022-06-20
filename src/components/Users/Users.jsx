f10bef36828f12f01e9701e98bbb3f9e0c9af0f0
Archoza <geofiziks@mail.ru>
2022-06-20 22:53:41 +0300

    54 componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,12 +3,10 @@ import classes from './users.module.css'
 import axios from "axios";
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => this.props.setUsers(response.data.items))
-        }
-
+    }
 
 
     render() {
