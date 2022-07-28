dc935de417605f52ec6998ca8885bcb751f2d4d8
Archoza <geofiziks@mail.ru>
2022-07-28 14:35:34 +0300

    95 add Router way

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ let AppContainer = compose(
 const SamuraiJSApp = () => {
     return (
         <React.StrictMode>
-            <Router>
+            <Router basename={process.env.PUBLIC_URL}>
                 <Provider store={store}>
                     <AppContainer/>
                 </Provider>
